094a5b2ff9467f892c6258494a3cb6b7b26ba6c8
yeeeqichen <yeeeqichen@gmail.com>
2020-08-04 11:45:46 +0800

    '完成项目模块化封装'

--- EntityLinker.py
+++ EntityLinker.py
@@ -1,13 +1,36 @@
 # coding=UTF-8
 """
 @description:
-    该文件对外提供entity_linker()方法，用于预测句子中的mention对应的entity
+    该文件实现entity_linker()方法，用于预测句子中的mention对应的entity
+    并封装了项目接口link（），接收文本和NER标签序列，返回mention以及其对应链接结果
 @author: yeeeqichen
 """
 from Config import config, embed
 from CandidateGenerator import get_candidates
+from Merge import merge_elements
 
 NIL = 'fail to link: bond kind not found in knowledge base!'
+
+
+def get_mentions(_blocks):
+    """
+    :param _blocks:每个block表示一只债券
+    :return: 每只债券的mention，及其对应的债券类型，如果没有则返回 #
+    """
+    _mentions = []
+    _bond_kinds = []
+    for block in _blocks:
+        mention = ''
+        flag = 0
+        for ele, kind in zip(block['elements'], block['tags']):
+            mention += ele
+            if kind == '债券类型':
+                _bond_kinds.append(ele)
+                flag = 1
+        _mentions.append(mention)
+        if flag == 0:
+            _bond_kinds.append('#')
+    return _mentions, _bond_kinds
 
 
 # todo:相似度不够高时，进行消岐(长尾，因为需要用到正文信息）
@@ -39,3 +62,18 @@ def entity_linker(sentence, mentions, kinds):
         entity_set.append(candidates[0][0])
         candidate_set.append(candidates)
     return candidate_set, entity_set
+
+
+def link(text, tags):
+    """
+    :param text: 待链接的mention所在的文本
+    :param tags: NER标注序列
+    :return: text中的mention以及其对应的链接结果
+    """
+    blocks = merge_elements(text, tags)
+    mention_set, kind_set = get_mentions(blocks)
+    if len(mention_set) == 0:
+        return 'no mention'
+    candidates, predicts = entity_linker(text, mention_set, kind_set)
+    for mention, _, predict in zip(mention_set, candidates, predicts):
+        yield {'mention': mention, 'predict': predict[:-1]}
--- main.py
+++ main.py
@@ -1,29 +1,7 @@
 # coding=UTF-8
 from Config import config
-from EntityLinker import entity_linker
+from EntityLinker import link
 import json
-from Merge import merge_elements
-
-
-def get_mentions(_blocks):
-    """
-    :param _blocks:每个block表示一只债券
-    :return: 每只债券的mention，及其对应的债券类型，如果没有则返回 #
-    """
-    _mentions = []
-    _bond_kinds = []
-    for block in _blocks:
-        mention = ''
-        flag = 0
-        for ele, kind in zip(block['elements'], block['tags']):
-            mention += ele
-            if kind == '债券类型':
-                _bond_kinds.append(ele)
-                flag = 1
-        _mentions.append(mention)
-        if flag == 0:
-            _bond_kinds.append('#')
-    return _mentions, _bond_kinds
 
 
 if __name__ == '__main__':
@@ -38,15 +16,8 @@ if __name__ == '__main__':
     for text, tags in zip(texts, tags):
         print('*' * 20)
         print('text: ', text)
-        blocks = merge_elements(text, tags)
-        mention_set, kind_set = get_mentions(blocks)
-        if len(mention_set) == 0:
-            print('no mention')
-        else:
-            candidates, predicts = entity_linker(text, mention_set, kind_set)
-            for mention, candidate, predict in zip(mention_set, candidates, predicts):
-                print('mention: ', mention)
-                print('predict: ', predict)
-                # print('candidate: ', candidate)
+        for result in link(text, tags):
+            print(result)
+
 
 
